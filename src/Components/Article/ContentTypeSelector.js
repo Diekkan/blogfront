fbb055fac928349b75617ae740b500bcc83281f7
Diekkan <merentiel.diego@gmail.com>
2022-08-31 20:02:20 -0300

    First components

---
+++ src/Components/Article/ContentTypeSelector.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ContentTypeSelector = () => {
+    const contentTypes = ['text', ];
+    return(
+        <div className='content-type-selector'>
+            <div id='text'></div>
+        </div>
+    )
+}
